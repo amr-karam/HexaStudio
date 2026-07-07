1a716ec3cf28bb424f9bec3549ded820bc99f8e1
Cap. Amr Karam <info@justfilmk.online>
2026-07-08 00:40:13 +0300

    fix: use 'user' instead of 'username' in CMS database config for Strapi 5 pg driver

--- apps/cms/config/database.js
+++ apps/cms/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "postgres"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "hexastudio_cms"),
-      username: env("DATABASE_USERNAME", "hexastudio"),
+      user: env("DATABASE_USERNAME", "hexastudio"),
       password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false),
     },
