369fe67a8fb4e61db8e367474719d17476339ac7
Cap. Amr Karam <info@justfilmk.online>
2026-07-13 22:59:02 +0300

    fix: CMS - correct node_modules path in start.js

--- apps/cms/start.js
+++ apps/cms/start.js
@@ -1,7 +1,7 @@
 const path = require('path');
 process.env.NODE_ENV = 'production';
 
-const strapi = require(path.resolve(__dirname, '../node_modules/@strapi/strapi'));
+const strapi = require(path.resolve(__dirname, '../../node_modules/@strapi/strapi'));
 
 const app = strapi.createStrapi({
   appDir: __dirname,
