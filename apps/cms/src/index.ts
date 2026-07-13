8e69ef03b80c61e0c4a44bdc69fa2254f17e13ee
Cap. Amr Karam <info@justfilmk.online>
2026-07-13 20:48:22 +0300

    fix: CMS - remove empty routes override from index.ts so Strapi auto-discovers content types

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -1,7 +1,1 @@
-export default {
-  /**
-   * HexaStudio CMS content types will be added here:
-   * - blog, categories, testimonials, portfolio, services, SEO content
-   */
-  routes: [],
-};
+export default {};
