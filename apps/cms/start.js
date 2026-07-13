7acd65170772b3b8050b9ce4d87f5af53a4805b6
Cap. Amr Karam <info@justfilmk.online>
2026-07-13 23:08:51 +0300

    fix: CMS - use absolute path for @strapi/strapi in start.js

--- apps/cms/start.js
+++ apps/cms/start.js
@@ -1,7 +1,7 @@
 const path = require('path');
 process.env.NODE_ENV = 'production';
 
-const strapi = require('@strapi/strapi');
+const strapi = require('/app/node_modules/@strapi/strapi');
 
 const app = strapi.createStrapi({
   appDir: __dirname,
