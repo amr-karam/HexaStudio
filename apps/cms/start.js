b6ed642a857fe3e796c6ba470acb098712b310eb
Cap. Amr Karam <info@justfilmk.online>
2026-07-13 22:06:10 +0300

    fix: CMS - keep dist/src for Strapi content type discovery (dirs.dist.api = dist/src/api)

---
+++ apps/cms/start.js
@@ -0,0 +1,11 @@
+const path = require('path');
+process.env.NODE_ENV = 'production';
+
+const strapi = require(path.resolve(__dirname, '../node_modules/@strapi/strapi'));
+
+const app = strapi.createStrapi({
+  appDir: __dirname,
+  distDir: path.join(__dirname, 'dist'),
+});
+
+app.start();
